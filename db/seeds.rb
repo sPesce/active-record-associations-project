e95b22134e253d6b827cb19bbd29be41c58545a9
steve <stevepesce879@gmail.com>
2020-06-15 00:03:52 -0400

    created seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,23 @@
-users =
-[
-  User.create(name: "fred"),
-  User.create(name: "Bob"),
+User.destroy_all
+Category.destroy_all
+Book.destroy_all
+BookRental.destroy_all
+Author.destroy_all
+
+  User.create(name: "fred")
+  User.create(name: "Bob")
   User.create(name: "Andy")
-]
-books =
-[
-  Book.create(title: "The Book"),
-  Book.create(title: "Das Book"),
+
+  Book.create(title: "The Book")
+  Book.create(title: "Das Book")
   Book.create(title: "El Book")
-]
-authors =
-[
-  Author.create(name: "Mr Author"),
-  Author.create(name: "Mrs Author"),
+
+  Author.create(name: "Mr Author")
+  Author.create(name: "Mrs Author")
   Author.create(name: "Arthur Authorington")
-]
-categories =
-[
-  Category.create(name: "Mystery"),
-  Category.create(name: "Autobiography"),
+
+
+  Category.create(name: "Mystery")
+  Category.create(name: "Autobiography")
   Category.create(name: "Fiction")
-]
+
